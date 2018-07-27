77e4c926c2500b139a5c927a09b382094cca00d2
XorANdre <xor2017@outlook.com>
2018-07-27 12:56:04 -0300

    Upload de arquivosok

---
+++ src/assets/js/components.js
@@ -0,0 +1,16 @@
+//Toglle
+function toggle_action() {
+    const element = document.getElementById("Toggle");
+    if (element.classList) { 
+        element.classList.toggle("reveal");
+    } else {
+        const classes = element.className.split(" ");
+        const i = classes.indexOf("reveal");
+
+        if (i >= 0) 
+            classes.splice(i, 1);
+        else 
+            classes.push("reveal");
+            element.className = classes.join(" "); 
+    }
+}
